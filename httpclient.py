774298b3a5da3b6f147d4b66cf8823960f3ccabb
genevag <geneva@ualberta.ca>
2016-01-31 02:11:54 -0700

    passes all freetests. doesn't handle boundary cases yet.

--- httpclient.py
+++ httpclient.py
@@ -25,6 +25,7 @@ import re
 # you may use urllib to encode data appropriately
 import urllib
 import json
+import select
 
 def help():
     print "httpclient.py [GET/POST] [URL]\n"
@@ -35,48 +36,54 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    # try using regular expressions to parse the url
     def get_host_port(self,url):
-        if url[:7] == "http://":
-            url = url[7:]
-        elif url[:8] == "https://":
-            url = url[8:]
+        print url
 
-        try:
-            host, others = url.split(':', 1)
-        except:
-            host, endpoint = url.split("/", 1)
-            # default port if not given
+        # Rules taken from : https://regex101.com/r/aO0wR7/2 2016-01-29
+        rules = "(?P<Protocol>[a-zA-Z]+\://)?(?P<Host>(?:(?:(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d?|0)\.){3}(?:25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d?|0))|[a-zA-Z0-9\-\.]*\.+[a-zA-Z]*)(?P<Port>:(?:6553[0-5]|655[0-2]\d|65[0-4]\d{2}|6[0-4]\d{3}|[1-5]\d{4}|[1-9]\d{0,3}))?(?P<Location>/(?:[a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~]*)[^\.\,\)\(\s]?)?"
+        a = re.match(rules, url)
+
+        httpProtocol = a.group("Protocol")
+        host = a.group("Host")
+        port = a.group("Port")
+        location = a.group("Location")
+
+        if not port:
             port = 80
         else:
-            port, endpoint = others.split("/", 1)
-        
-        endpoint = "/" + endpoint
+            port = int(port[1:])
 
-        return (host, int(port), endpoint)
+        if not location:
+            location = '/'
+
+        return (host, port, location)
 
     def connect(self, host, port):
         # use sockets!
         cs = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print "-- Connect --"
-        print (host,port)
-        print "-------------"
+        # print "-- Connect --"
+        # print (host,port)
+        # print "-------------"
         cs.connect((host, port))
         return cs
 
     def get_code(self, data):
-        statusLine, others = data.split("\n", 1)
-        httpVersion, statusCode, description = statusLine.split(" ", 2)
-        print "\n\nCode: " + statusCode
+        # print "DATA : " + data
+        if data:
+            statusLine, others = data.split("\n",1)
+            httpVersion, statusCode, description = statusLine.split(" ", 2)
+        # print "\n\nCode: " + statusCode
         return int(statusCode)
 
     def get_headers(self,data):
-        return None
+        header, body = data.split("\r\n\r\n", 1)
+        return header
 
     def get_body(self, data):
-        index = data.find('\r\n\r\n')
-        body = data[index+4:]
-        print "\n\nBody:\n" + body
+        header, body = data.split("\r\n\r\n", 1)
+        # index = data.find('\r\n\r\n')
+        # body = data[index+4:]
+        # print "\n\nBody:\n" + body
         return body
 
 
@@ -85,12 +92,17 @@ class HTTPClient(object):
         response = bytearray()
         done = False
         while not done:
-            # sock.setblocking(0)
+            sock.setblocking(0)
+            (readable, wr, ex) = select.select([sock], [], [], 5)   #http://www.gossamer-threads.com/lists/python/python/81096 Author: jknapka at earthlink 2016-01-27
+            if readable :
+                part = sock.recv(1024)
 
-            part = sock.recv(1024)
-
+            # print part
             if (part):
+                print part
                 response.extend(part)
+                if "Content-Length: 0\r\n" in part:
+                    done = True
             else:
                 done = not part
 
@@ -100,7 +112,7 @@ class HTTPClient(object):
         request = "GET " + endpoint + " HTTP/1.1\n" + \
                   "Host: " + host + ":" + str(port) + "\r\n\r\n"
 
-        print "Request:\n" + request
+        # print "Request:\n" + request
         return request
 
     # make port default to 80 if not given?
@@ -112,19 +124,22 @@ class HTTPClient(object):
 
     def GET(self, url, args=None):
         (host, port, endpoint) = self.get_host_port(url)
-        print "Host: %s\nPort: %d\nEndpoint: %s" % (host, port, endpoint)
+        # print "Host: %s\nPort: %d\nEndpoint: %s\n" % (host, port, endpoint)
 
         request = self.formulateGETRequest(host, port, endpoint)
         clientSocket = self.sendRequest(request, host, port)
-        response = self.recvall(clientSocket)
-        print "\n\nResponse: \n" + response
 
-        code = self.get_code(response)
+        response = self.recvall(clientSocket)
+        # print "\n\nResponse: \n" + response
+
+        headers = self.get_headers(response)
+        code = self.get_code(headers)
         body = self.get_body(response)
 
-        print "--- Code : %d ---" % int(code)
-        print "--- Body : \n%s ---" % body
+        # print "--- Code : %d ---" % int(code)
+        # print "--- Body : \n%s ---" % body
         return HTTPResponse(code, body)
+ 
 
 
     def formulatePOSTRequest(self, host, port, endpoint, args):
@@ -139,25 +154,26 @@ class HTTPClient(object):
         request += "Content-Length: " + str(len(encoded_body)) + "\r\n\r\n"
         request += encoded_body
 
-        print '\n\n' + request
+        # print '\n\n' + request
         return request
 
     def POST(self, url, args=None):
-        print args
-        print len(json.dumps(args))
+        # print args
+        # print len(json.dumps(args))
 
         (host, port, endpoint) = self.get_host_port(url)
-        print "Host: %s\nPort: %d\nEndpoint: %s" % (host, port, endpoint)
+        # print "Host: %s\nPort: %d\nEndpoint: %s" % (host, port, endpoint)
+
         request = self.formulatePOSTRequest(host, port, endpoint, args)
         clientSocket = self.sendRequest(request, host, port)
         response = self.recvall(clientSocket)
-        print "\n\nResponse: \n\n\n" + response
+        # print "\n\nResponse: \n\n\n" + response
 
         code = self.get_code(response)
         body = self.get_body(response)
 
-        print "--- Code : %d ---" % int(code)
-        print "--- Body : \n%s ---" % body
+        # print "--- Code : %d ---" % int(code)
+        # print "--- Body : \n%s ---" % body
 
         return HTTPResponse(code, body)
 
